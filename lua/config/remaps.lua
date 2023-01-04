67d2365ce08f8cdfdca6e5c2e4789495d6e44ea6
Bruno Maioli <bmaioli@gmail.com>
2023-01-03 21:50:04 -0300

    first draft

---
+++ lua/config/remaps.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>P", ":Files<CR>")
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
