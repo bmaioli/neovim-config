37e3a7190e98db9232d5f5b524d9cd67cf6bcc72
Bruno Maioli <bmaioli@gmail.com>
2023-01-06 08:27:17 -0300

    changing indent for js files

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,13 @@
 require("config.opts")
 require("config.plugins")
 require("config.remaps")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+augroup("setIndent", { clear = true })
+autocmd("Filetype", {
+    group = "setIndent",
+    pattern = { "javascript", "typescript" },
+    command = "setlocal shiftwidth=2 tabstop=2"
+})
